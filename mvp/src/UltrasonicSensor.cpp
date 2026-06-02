54939415561ab6dbe9f85ec93ba0f0eb2b5264f2
dariaporay <daryaporay@gmail.com>
2026-06-02 22:04:13 +0300

    add UltrasonicSensor

--- mvp/src/UltrasonicSensor.cpp
+++ mvp/src/UltrasonicSensor.cpp
@@ -1,8 +1,8 @@
 #include "UltrasonicSensor.h"
-
 #include "pico/stdlib.h"
 
-UltrasonicSensor::UltrasonicSensor(unsigned trig, unsigned echo) : trig_(trig), echo_(echo)
+UltrasonicSensor::UltrasonicSensor(unsigned trig, unsigned echo)
+    : trig_(trig), echo_(echo)
 {
 }
 
@@ -10,100 +10,58 @@ void UltrasonicSensor::init()
 {
     gpio_init(trig_);
     gpio_set_dir(trig_, GPIO_OUT);
+    gpio_put(trig_, 0);
 
     gpio_init(echo_);
     gpio_set_dir(echo_, GPIO_IN);
 
-    gpio_put(trig_, 0);
-
-    state_ = State::IDLE;
+    ready_ = false;
+    error_ = false;
+    distance_ = -1.0f;
 }
 
 void UltrasonicSensor::update()
 {
-    uint32_t now = time_us_32();
+    ready_ = false;
+    error_ = false;
 
-    switch(state_)
+    gpio_put(trig_, 0);
+    sleep_us(2);
+    gpio_put(trig_, 1);
+    sleep_us(10);
+    gpio_put(trig_, 0);
+
+    uint32_t start_wait = time_us_32();
+    while (!gpio_get(echo_))
     {
-        case State::IDLE:
+        if (time_us_32() - start_wait > 30000)
         {
-            ready_ = false;
-
-            gpio_put(trig_, 1);
-
-            stateStart_ = now;
-
-            state_ = State::TRIGGER_HIGH;
-
-            break;
-        }
-
-        case State::TRIGGER_HIGH:
-        {
-            if(now - stateStart_ >= 10)
-            {
-                gpio_put(trig_, 0);
-
-                stateStart_ = now;
-
-                state_ = State::WAIT_RISE;
-            }
-
-            break;
-        }
-
-        case State::WAIT_RISE:
-        {
-            if(gpio_get(echo_))
-            {
-                echoStart_ = now;
-
-                state_ = State::WAIT_FALL;
-            }
-
-            else if(now - stateStart_ > 30000)
-            {
-                error_ = true;
-
-                distance_ = -1;
-
-                ready_ = true;
-
-                state_ = State::IDLE;
-            }
-
-            break;
-        }
-
-        case State::WAIT_FALL:
-        {
-            if(!gpio_get(echo_))
-            {
-                uint32_t echoTime = now - echoStart_;
-
-                distance_ = echoTime * 0.0343f / 2.0f;
-
-                ready_ = true;
-
-                error_ = false;
-
-                state_ = State::IDLE;
-            }
-
-            else if(now - echoStart_ > 30000)
-            {
-                error_ = true;
-
-                distance_ = -1;
-
-                ready_ = true;
-
-                state_ = State::IDLE;
-            }
-
-            break;
+            error_ = true;
+            distance_ = -1.0f;
+            ready_ = true;
+            return;
         }
     }
+
+    uint32_t echo_start = time_us_32();
+
+    while (gpio_get(echo_))
+    {
+        if (time_us_32() - echo_start > 30000)
+        {
+            error_ = true;
+            distance_ = -1.0f;
+            ready_ = true;
+            return;
+        }
+    }
+
+    uint32_t echo_end = time_us_32();
+    uint32_t echo_time = echo_end - echo_start;
+
+    distance_ = echo_time * 0.0343f / 2.0f;
+    ready_ = true;
+    error_ = false;
 }
 
 bool UltrasonicSensor::measurementReady() const
@@ -121,32 +79,19 @@ bool UltrasonicSensor::hasError() const
     return error_;
 }
 
-bool UltrasonicSensor::gestureDetected(
-    float thresholdCm)
+bool UltrasonicSensor::gestureDetected(float thresholdCm)
 {
-    if(!ready_)
-    {
+    if (!ready_ || error_)
         return false;
-    }
-
-    if(error_)
-    {
-        return false;
-    }
 
     uint32_t now = to_ms_since_boot(get_absolute_time());
 
-    if(distance_ > thresholdCm)
-    {
+    if (distance_ > thresholdCm)
         return false;
-    }
 
-    if(now - lastGestureTime_ < GESTURE_COOLDOWN_MS)
-    {
+    if (now - lastGestureTime_ < GESTURE_COOLDOWN_MS)
         return false;
-    }
 
     lastGestureTime_ = now;
-
     return true;
 }
